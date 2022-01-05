ec32b3cb110aebb458e9734557d2e24eb69b3886
Stephen Oberther <stephenoberther@gmail.com>
2022-01-04 19:25:43 -0500

    add metric as a source

--- demo/stats/main.go
+++ demo/stats/main.go
@@ -4,21 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strings"
 
 	"ar/internal/generator"
+	"ar/internal/sources"
 	"ar/internal/types"
 )
-
-const maxBufferSize = 1024
 
 func main() {
 	done := make(chan string)
 	defer close(done)
 	src := fmt.Sprintf("127.0.0.1:%d", 12345)
 	dest := fmt.Sprintf("127.0.0.1:%d", 23456)
-	forwarder(done, dest, reader(done, src))
-	r := reader(done, dest)
+	forwarder(done, dest, sources.Metrics(done, src))
+	r := sources.Metrics(done, dest)
 
 	generator.MetricStream(done, src)
 	for m := range r {
@@ -26,64 +24,12 @@ func main() {
 	}
 }
 
-func openPort(addr string) (*net.UDPAddr, error) {
-	return net.ResolveUDPAddr("udp", addr)
-}
-
 func dial(addr string) (*net.UDPConn, error) {
-	port, err := openPort(addr)
+	port, err := net.ResolveUDPAddr("udp", addr)
 	if err != nil {
 		return nil, err
 	}
 	return net.DialUDP("udp", nil, port)
-}
-
-func listen(addr string) (*net.UDPConn, error) {
-	port, err := openPort(addr)
-	if err != nil {
-		return nil, err
-	}
-	return net.ListenUDP("udp", port)
-}
-
-// reads metrcis from addr and sends to returned channel
-func reader(done chan string, addr string) <-chan types.Metric {
-	metricStream := make(chan types.Metric)
-	go func() {
-		defer close(metricStream)
-
-		in, err := listen(addr)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer in.Close()
-
-		buffer := make([]byte, maxBufferSize)
-
-		for {
-			select {
-			case <-done:
-				return
-			default:
-				_, err := in.Read(buffer)
-				if err != nil {
-					fmt.Println("Error", err)
-					return
-				}
-
-				// split on new line and write to channel
-				i := strings.LastIndex(string(buffer), "\n")
-				lines := strings.Split(string(buffer[:i]), "\n")
-
-				buffer = make([]byte, maxBufferSize)
-				for _, line := range lines {
-					metricStream <- types.MetricFromStatsd(line)
-				}
-			}
-		}
-
-	}()
-	return metricStream
 }
 
 func forwarder(done chan string, addr string, metricStream <-chan types.Metric) {
---
+++ internal/sources/metrics.go
@@ -0,0 +1,60 @@
+package sources
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	"ar/internal/types"
+)
+
+const maxBufferSize = 1024
+
+func listen(addr string) (*net.UDPConn, error) {
+	port, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return net.ListenUDP("udp", port)
+}
+
+// reads metrcis from addr and sends to returned channel
+func Metrics(done chan string, addr string) <-chan types.Metric {
+	metricStream := make(chan types.Metric)
+	go func() {
+		defer close(metricStream)
+
+		in, err := listen(addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer in.Close()
+
+		buffer := make([]byte, maxBufferSize)
+
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				_, err := in.Read(buffer)
+				if err != nil {
+					fmt.Println("Error", err)
+					return
+				}
+
+				// split on new line and write to channel
+				i := strings.LastIndex(string(buffer), "\n")
+				lines := strings.Split(string(buffer[:i]), "\n")
+
+				buffer = make([]byte, maxBufferSize)
+				for _, line := range lines {
+					metricStream <- types.MetricFromStatsd(line)
+				}
+			}
+		}
+
+	}()
+	return metricStream
+}
